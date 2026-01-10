732cadd67a30a63b97172877f8f5e290e156afcf
ibtasaams <iabbasi.bee25seecs@seecs.edu.pk>
2026-01-10 12:03:34 +0500

    cls

--- basics/1.cpp
+++
@@ -1,8 +0,0 @@
-// Write your first program saying “Hello World”. Now change the program to print your name and roll number instead.
-#include <iostream>
-using namespace std;
-int main(){
-    cout << "Hello World" << endl;
-    cout << 540642;
-    return 0;
-}
